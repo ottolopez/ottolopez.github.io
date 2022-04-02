fbb56cf2a9167e3ff7b34b90303261f82e74c042
ottolopez <holaotto@yahoo.com>
2022-04-02 01:00:34 -0600

    1st backup of the old website

---
+++ pim/partnersinmissions/aspnet_client/system_web/1_1_4322/_vti_cnf/SmartNav.js
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Apr 2010 18:36:44 -0000
+vti_extenderversion:SR|5.0.2.4803
+vti_cacheddtm:TX|14 Apr 2010 18:36:44 -0000
+vti_filesize:IR|8332
+vti_backlinkinfo:VX|
